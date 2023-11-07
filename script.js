238c50e949f88d246d5ab191cdcc999f8728c007
bschwar9 <49416461+bschwar9@users.noreply.github.com>
2023-11-07 17:50:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,30 @@ function toggleSpinningEffect() {
     img.classList.toggle('spinning');
 }
 
-// Event listener for clicks on the document body
+// Event listener for clicks on the document body to toggle image spinning
 document.body.addEventListener('click', toggleSpinningEffect);
+
+// Function to fetch and play the latest audio
+function playLatestAudio() {
+    fetch('https://1mkbfmthe9.execute-api.us-east-2.amazonaws.com/getLatestAudio')
+        .then(response => {
+            if (response.ok) {
+                return response.blob();
+            }
+            throw new Error('Network response was not ok.');
+        })
+        .then(blob => {
+            const audioUrl = URL.createObjectURL(blob);
+            const audio = new Audio(audioUrl);
+            audio.play();
+        })
+        .catch(error => {
+            console.error('There has been a problem with your fetch operation:', error);
+        });
+}
+
+// Event listener for the audio button
+document.getElementById('audioButton').addEventListener('click', playLatestAudio);
 
 // This function will run when the DOM is fully loaded
 window.addEventListener('DOMContentLoaded', (event) => {
@@ -13,7 +35,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     fetch('https://1mkbfmthe9.execute-api.us-east-2.amazonaws.com/getLatestImage')
         .then(response => {
             if (response.ok) {
-                return response.blob(); // Assuming the response is an image blob
+                return response.blob();
             }
             throw new Error('Network response was not ok.');
         })
