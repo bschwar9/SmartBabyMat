33afa1e81d8b7cd868e0650fcdb07af0ccc31a56
bschwar9 <49416461+bschwar9@users.noreply.github.com>
2023-11-07 16:33:54 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    fetch('https://1mkbfmthe9.execute-api.us-east-2.amazonaws.com/getLatestImage')
+        .then(response => {
+            if(response.ok) {
+                return response.blob(); // Assuming the response is an image blob
+            }
+            throw new Error('Network response was not ok.');
+        })
+        .then(blob => {
+            const imageUrl = URL.createObjectURL(blob);
+            document.getElementById('raspberryImage').src = imageUrl;
+        })
+        .catch(error => {
+            console.error('There has been a problem with your fetch operation:', error);
+        });
+});
