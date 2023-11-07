c380e5b83c7349f8070e9e83c3f2496cf6e599fb
bschwar9 <49416461+bschwar9@users.noreply.github.com>
2023-11-07 16:59:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     fetch('https://1mkbfmthe9.execute-api.us-east-2.amazonaws.com/getLatestImage')
         .then(response => {
-            if(response.ok) {
+            if (response.ok) {
                 return response.blob(); // Assuming the response is an image blob
             }
             throw new Error('Network response was not ok.');
