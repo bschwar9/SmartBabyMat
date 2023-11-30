3c1bd7d5ef32464385200a0281e66aa1c0b537e3
bschwar9 <49416461+bschwar9@users.noreply.github.com>
2023-11-30 14:12:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,54 @@
-// Function to toggle spinning animation on the image
-function toggleSpinningEffect() {
-    var img = document.getElementById('raspberryImage');
-    img.classList.toggle('spinning');
-}
-
 // Event listener for the image to toggle spinning effect
-document.getElementById('raspberryImage').addEventListener('click', toggleSpinningEffect);
+document.addEventListener('DOMContentLoaded', function() {
+    var img = document.getElementById('raspberryImage');
+    img.addEventListener('click', function() {
+        // Toggle the spinning effect on the image
+        this.classList.toggle('spinning');
+    });
 
-var audio = new Audio();
+    var audio = new Audio();
 
-// Function to fetch and play the latest audio
-function playLatestAudio() {
-    if (!audio.paused) {
-        audio.pause();
-        audio.currentTime = 0;
+    // Function to fetch and play the latest audio
+    document.getElementById('audioButton').addEventListener('click', function() {
+        fetch('https://1mkbfmthe9.execute-api.us-east-2.amazonaws.com/getLatestAudio')
+            .then(response => {
+                if (response.ok) {
+                    return response.blob();
+                }
+                throw new Error('Network response was not ok.');
+            })
+            .then(blob => {
+                const audioUrl = URL.createObjectURL(blob);
+                audio.src = audioUrl;
+                audio.play();
+            })
+            .catch(error => {
+                console.error('There has been a problem with your fetch operation:', error);
+            });
+    });
+
+    // Function to fetch and display weight data
+    function displayWeightData() {
+        const startDate = '2023-11-04T16:22:14.452559';
+        const endDate = '2023-11-04T17:52:14.452559';
+        const apiUrl = `https://1mkbfmthe9.execute-api.us-east-2.amazonaws.com/getWeights?startDate=${startDate}&endDate=${endDate}`;
+
+        fetch(apiUrl)
+            .then(response => response.ok ? response.json() : Promise.reject('Failed to load'))
+            .then(data => {
+                const tableBody = document.getElementById('weightTable').querySelector('tbody');
+                let rows = '';
+                data.forEach(item => {
+                    rows += `<tr><td>${item.date}</td><td>${item.weight}</td></tr>`;
+                });
+                tableBody.innerHTML = rows;
+            })
+            .catch(error => {
+                console.error('There was a problem with fetching weight data:', error);
+            });
     }
 
-    fetch('https://1mkbfmthe9.execute-api.us-east-2.amazonaws.com/getLatestAudio')
-        .then(response => {
-            if (response.ok) {
-                return response.blob();
-            }
-            throw new Error('Network response was not ok.');
-        })
-        .then(blob => {
-            const audioUrl = URL.createObjectURL(blob);
-            audio.src = audioUrl;
-            audio.play();
-        })
-        .catch(error => {
-            console.error('There has been a problem with your fetch operation:', error);
-        });
-}
-
-// Event listener for the audio button
-document.getElementById('audioButton').addEventListener('click', playLatestAudio);
-
-// Function to fetch and display weight data
-function displayWeightData() {
-    const startDate = '2023-11-04T16:22:14.452559';
-    const endDate = '2023-11-04T17:52:14.452559';
-    const apiUrl = `https://1mkbfmthe9.execute-api.us-east-2.amazonaws.com/getWeights?startDate=${startDate}&endDate=${endDate}`;
-
-    fetch(apiUrl)
-        .then(response => response.ok ? response.json() : Promise.reject('Failed to load'))
-        .then(data => {
-            const tableBody = document.getElementById('weightTable').querySelector('tbody');
-            let rows = '';
-            data.forEach(item => {
-                rows += `<tr><td>${item.date}</td><td>${item.weight}</td></tr>`;
-            });
-            tableBody.innerHTML = rows;
-        })
-        .catch(error => {
-            console.error('There was a problem with fetching weight data:', error);
-        });
-}
-
-// This function will run when the DOM is fully loaded
-window.addEventListener('DOMContentLoaded', (event) => {
+    // Fetch and display the latest image from the Raspberry Pi
     fetch('https://1mkbfmthe9.execute-api.us-east-2.amazonaws.com/getLatestImage')
         .then(response => {
             if (response.ok) {
@@ -68,10 +58,11 @@ window.addEventListener('DOMContentLoaded', (event) => {
         })
         .then(blob => {
             const imageUrl = URL.createObjectURL(blob);
-            document.getElementById('raspberryImage').src = imageUrl;
+            img.src = imageUrl;
         })
         .catch(error => {
             console.error('There has been a problem with your fetch operation:', error);
         });
+
     displayWeightData();
 });
